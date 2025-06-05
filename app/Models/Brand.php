dd55ba97503254942cdca1348039028a0aead33e
rsahu93 <rsahu8817@gmail.com>
2025-06-06 01:36:11 +0530

    chnage path

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -38,6 +38,6 @@ class Brand extends Model
 
     public function getBrandImageAttribute($value)
     {
-        return asset('storage/brand/' . $value); // Or use Storage::url() if stored via Laravel storage
+        return asset($value); // Or use Storage::url() if stored via Laravel storage
     }
 }
