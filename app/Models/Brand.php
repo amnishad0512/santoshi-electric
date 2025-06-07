85c70e99f65347a9d182a025d0b88fe6e0dc5503
Prince <princu2212@gmail.com>
2025-06-08 00:29:58 +0530

    refactor: correct URL path in getBrandImageAttribute method

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -39,6 +39,6 @@ class Brand extends Model
 
     public function getBrandImageAttribute($value)
     {
-        return url('api/public/storage/' . $value);
+        return url('public/storage/' . $value);
     }
 }
