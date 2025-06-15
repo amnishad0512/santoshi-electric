c2e4bda4ad12cc818eca3fcf34a96e12bcff5a47
rsahu93 <rsahu8817@gmail.com>
2025-06-16 01:52:51 +0530

    feat: add image

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -45,10 +45,10 @@ class Brand extends Model
     public function getBrandImageAttribute($value)
     {
         if ($value) {
-            return url('storage/' . $value); 
+            return url('public/storage/' . $value); 
         }
 
-        return asset('images/default_product.jpg'); 
+        return asset('public/images/default_product.jpg'); 
         // return url('public/storage/' . $value);
         
     }
