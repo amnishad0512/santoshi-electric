3f44acfb7ac1655c8f61b64b05736abdbbb81938
Prince <princu2212@gmail.com>
2025-06-16 01:50:16 +0530

    fix: update image storage path in BrandController

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -148,7 +148,7 @@ class BrandController extends Controller
                     }
                 }
                 $imagePath = $request->file('brand_image')->store('brands', 'public');
-                $save_url = 'storage/' . $imagePath;
+                $save_url = $imagePath;
             }
 
             $brand->update([
